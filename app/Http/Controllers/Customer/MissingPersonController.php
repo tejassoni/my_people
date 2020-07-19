50097eca8b7a3a9ea61ceb89e9592242cc069c23
Tejas Soni <tejas.soni@sinelogix.com>
2020-07-19 16:16:45 +0530

    missing person update

--- app/Http/Controllers/Customer/MissingPersonController.php
+++ app/Http/Controllers/Customer/MissingPersonController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Customer;
 
 use DataTables;
 use App\Models\missing_person;
--- resources/views/customer/missing_persons/add_view.blade.php
+++ resources/views/customer/missing_persons/add_view.blade.php
@@ -46,36 +46,150 @@
             <!-- csrf security ends -->
             <div class="card-body">
               <div class="form-group required">
-                <label for="discount_name"><?= ('Name') ?></label>
-                <input type="text" class="form-control" name="discount_name" id="discount_name" placeholder="Enter Missing Person Name" value="{{ old('discount_name') }}" required />
+                <label for="first_name"><?= ('First Name') ?></label>
+                <input type="text" class="form-control" name="first_name" id="first_name" placeholder="Enter First Name" value="{{ old('first_name') }}" required />
+              </div>
+              <div class="form-group required">
+                <label for="middle_name"><?= ('Middle Name') ?></label>
+                <input type="text" class="form-control" name="middle_name" id="middle_name" placeholder="Enter Middle Name" value="{{ old('middle_name') }}" required />
+              </div>
+              <div class="form-group required">
+                <label for="last_name"><?= ('Last Name') ?></label>
+                <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Enter Last Name" value="{{ old('last_name') }}" required />
               </div>
 
               <div class="form-group required">
-                <label for="discount_description"><?= ('Description') ?></label>
-                <textarea class="form-control" name="discount_description" id="discount_description" rows="3" placeholder="Enter Missing Person Description" required>{{ old('discount_description') }}</textarea>
+                <label for="birth_date"> Birth Date </label>
+                <input type="text" class="form-control" name="birth_date" id="birth_date" placeholder="Enter Birth Date" value="{{ old('birth_date') }}" />
+              </div>
+
+              <div class="form-group required">
+                <label for="age"><?= ('Age') ?></label>
+                <input type="number" class="form-control" name="age" id="age" placeholder="Enter Age" value="{{ old('age') }}" required min="0" />
+              </div>
+
+              <div class="form-group required">
+                <label for="address"><?= ('Address') ?></label>
+                <textarea class="form-control" name="address" id="address" rows="3" placeholder="Enter Address" required>{{ old('address') }}</textarea>
               </div>
 
               <div class="form-group w-120 required">
-                <label><?= ('Select Missing Person Type') ?></label>
-                <select class="form-control" name="discount_type_select" id="discount_type_select" required>
-                  <option value="" disabled selected><?= ('Select Missing Person Type') ?></option>
+                <label><?= ('Select Country') ?></label>
+                <select class="form-control" name="country_select" id="country_select" required>
+                  <option value="" disabled selected><?= ('Select Country') ?></option>
                   <option value="none"><?= ('None') ?></option>
-                  <option value="fixed"><?= ('Fixed / Flat') ?></option>
-                  <option value="percentage"><?= ('Percentage') ?></option>
                 </select>
               </div>
 
-              <div class="form-group has-error">
-                <label for="discount_amount"><?= ('Missing Person Amount') ?></label>
-                <input type="number" class="form-control" min="0" name="discount_amount" id="discount_amount" placeholder="Enter Missing Person Amount" value="{{ (is_array(old()) && !empty(old('discount_amount')))? old('discount_amount') : 0 }}" disabled />
+              <div class="form-group w-120 required">
+                <label><?= ('Select State') ?></label>
+                <select class="form-control" name="state_select" id="state_select" required>
+                  <option value="" disabled selected><?= ('Select State') ?></option>
+                  <option value="none"><?= ('None') ?></option>
+                </select>
               </div>
 
-              <div class="form-group clearfix">
-                <div class="icheck-primary d-inline">
-                  <input type="checkbox" name="discount_validity_chkbx" class="discount_validity_chkbx" id="discount_validity_chkbx" value="0">
-                  <label for="discount_validity_chkbx"> Date Range Available </label>
-                </div>
-                <input type="text" class="form-control" name="discount_validity" id="discount_validity" placeholder="Enter Missing Person Validity" value="{{ old('discount_validity') }}" disabled />
+              <div class="form-group w-120 required">
+                <label><?= ('Select City') ?></label>
+                <select class="form-control" name="city_select" id="city_select" required>
+                  <option value="" disabled selected><?= ('Select City') ?></option>
+                  <option value="none"><?= ('None') ?></option>
+                </select>
+              </div>
+
+              <div class="form-group required">
+                <label for="pincode"><?= ('Pincode') ?></label>
+                <input type="number" class="form-control" name="pincode" id="pincode" placeholder="Enter Pincode" value="{{ old('pincode') }}" required />
+              </div>
+
+              <div class="form-group required">
+                <label for="missing_date"> Missing Date </label>
+                <input type="text" class="form-control" name="missing_date" id="missing_date" placeholder="Enter Missing Date" value="{{ old('missing_date') }}" />
+              </div>
+
+              <div class="form-group w-120">
+                <label><?= ('Select Hair') ?></label>
+                <select class="form-control" name="hair_select" id="hair_select" required>
+                  <option value="" disabled selected><?= ('Select Hair') ?></option>
+                  <option value="none"><?= ('None') ?></option>
+                </select>
+              </div>
+
+              <div class="form-group w-120">
+                <label><?= ('Select Eye') ?></label>
+                <select class="form-control" name="eye_select" id="eye_select" required>
+                  <option value="" disabled selected><?= ('Select Eye') ?></option>
+                  <option value="none"><?= ('None') ?></option>
+                </select>
+              </div>
+
+              <div class="form-group w-120">
+                <label><?= ('Select Eye Brow') ?></label>
+                <select class="form-control" name="eyebrow_select" id="eyebrow_select" required>
+                  <option value="" disabled selected><?= ('Select Eye Brow') ?></option>
+                  <option value="none"><?= ('None') ?></option>
+                </select>
+              </div>
+
+              <div class="form-group w-120">
+                <label><?= ('Select Lip') ?></label>
+                <select class="form-control" name="lip_select" id="lip_select" required>
+                  <option value="" disabled selected><?= ('Select Lip') ?></option>
+                  <option value="none"><?= ('None') ?></option>
+                </select>
+              </div>
+              <div class="form-group w-120">
+                <label><?= ('Select Face / Jaw') ?></label>
+                <select class="form-control" name="face_jaw_select" id="face_jaw_select" required>
+                  <option value="" disabled selected><?= ('Select Face Jaw') ?></option>
+                  <option value="none"><?= ('None') ?></option>
+                </select>
+              </div>
+              <div class="form-group w-120">
+                <label><?= ('Select Skin') ?></label>
+                <select class="form-control" name="skin_select" id="skin_select" required>
+                  <option value="" disabled selected><?= ('Select Skin') ?></option>
+                  <option value="none"><?= ('None') ?></option>
+                </select>
+              </div>
+              <div class="form-group w-120">
+                <label><?= ('Select Ear') ?></label>
+                <select class="form-control" name="ear_select" id="ear_select" required>
+                  <option value="" disabled selected><?= ('Select Ear') ?></option>
+                  <option value="none"><?= ('None') ?></option>
+                </select>
+              </div>
+
+              <div class="form-group w-120">
+                <label><?= ('Select Nose') ?></label>
+                <select class="form-control" name="nose_select" id="nose_select" required>
+                  <option value="" disabled selected><?= ('Select Nose') ?></option>
+                  <option value="none"><?= ('None') ?></option>
+                </select>
+              </div>
+
+
+              <div class="form-group required">
+                <label for="remark"><?= ('Remark') ?></label>
+                <textarea class="form-control" name="remark" id="remark" rows="3" placeholder="Enter Remark" required>{{ old('remark') }}</textarea>
+              </div>
+
+              <div class="form-group required">
+                <label for="remark"><?= ('Cloth Description') ?></label>
+                <textarea class="form-control" name="cloth_description" id="cloth_description" rows="3" placeholder="Enter Cloth Description" required>{{ old('cloth_description') }}</textarea>
+              </div>
+
+              <div class="form-group has-error">
+                <label for="reward_amount"><?= ('Reward Amount') ?></label>
+                <input type="number" class="form-control" min="0" name="reward_amount" id="reward_amount" placeholder="Enter Reward Amount" value="{{ (is_array(old()) && !empty(old('reward_amount')))? old('reward_amount') : 0 }}" />
+              </div>
+
+              <div class="form-group w-120">
+                <label><?= ('Select Currency Type For Reward') ?></label>
+                <select class="form-control" name="currency_select" id="currency_select" required>
+                  <option value="" disabled selected><?= ('Select Currency') ?></option>
+                  <option value="none"><?= ('None') ?></option>
+                </select>
               </div>
 
               <div class=" form-group">
